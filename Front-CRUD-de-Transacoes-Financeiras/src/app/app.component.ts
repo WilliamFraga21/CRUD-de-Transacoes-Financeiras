74eabf7af7fdf953f5d579db59f21828473ea463
WilliamFraga21 <william.fraga@fatec.sp.gov.br>
2024-11-04 22:19:58 -0300

    create front

--- Front-CRUD-de-Transacoes-Financeiras/src/app/app.component.ts
+++ Front-CRUD-de-Transacoes-Financeiras/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TesteService} from './_services/teste.service';
+// @ts-ignore
+import { Todo } from './_Models/teste.model'; // Importa o modelo
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Front-CRUD-de-Transacoes-Financeiras';
+  todos: Todo[] = [];
+
+  constructor(private testeService:  TesteService) {
+  }
+
+  ngOnInit() {
+    this.testeService.getall().subscribe((response) => {
+      this.todos = response; // Atribui a resposta ao array
+      console.log(this.todos);
+    });
+  }
+
+
+
+
+
+
 }
---
+++ Front-CRUD-de-Transacoes-Financeiras/src/config.ts
@@ -0,0 +1,4 @@
+// src/config.ts
+export const CONFIG = {
+  apiUrl: 'http://127.0.0.1:8000' // Substitua pelo IP e porta do backend
+};
